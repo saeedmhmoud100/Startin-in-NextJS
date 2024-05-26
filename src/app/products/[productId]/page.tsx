a185817e47bf0c38775e06e90bb9856ee214dd89
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-05-27 00:26:36 +0300

    metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,40 @@
-const page = ({ params } : {
+import {Metadata} from "next";
+
+type props = {
     params: {
         productId: string;
     };
-}) => {
+
+}
+
+export const generateMetadata = ({ params } : props): Metadata => {
+    return {
+        title: `Product ${params.productId} Details Page`,
+        description: 'Product Details Page',
+        keywords: 'Product Details, Product Details Page',
+    };
+
+}
+
+// with promise
+// export const generateMetadata =async ({ params } : props): Promise<Metadata> => {
+//
+//     const title = await new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(`Product ${params.productId} Details Page`);
+//         }, 1000);
+//     });
+//
+//     return {
+//         title: `Product ${title} Details Page`,
+//         description: 'Product Details Page',
+//         keywords: 'Product Details, Product Details Page',
+//     };
+//
+// }
+
+
+const page = ({ params } : props) => {
     return (
         <>Product Details ({params.productId})</>
     );
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,11 @@
+export const metadata = {
+    title: 'Products List Page',
+    description: 'Products List Page',
+    keywords: 'Products List, Products List Page',
+};
+
+
+
 const page = () => {
     return (
         <>Products List</>
