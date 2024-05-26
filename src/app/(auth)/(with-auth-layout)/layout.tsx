25ef418c76cd126950564f08e8a1f3ad83b037bb
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-05-26 21:54:45 +0300

    Route Group Layout

---
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Layout({ children }) {
+    return (
+        <div>
+        <h5>With Auth Layout</h5>
+        {children}
+        </div>
+    );
+}
---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ForgotPasswordPage() {
+    return (
+        <div>
+            <h1>Forgot Password</h1>
+        </div>
+    )
+}
