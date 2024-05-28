b5c7fc40ba51babda8384b8646790ee240720a8c
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-05-28 23:23:42 +0300

    Loading while routing

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,5 @@
 import {Metadata} from "next";
+import Link from "next/link";
 
 type props = {
     params: {
@@ -7,37 +8,44 @@ type props = {
 
 }
 
-export const generateMetadata = ({ params } : props): Metadata => {
-    return {
-        title: `Product ${params.productId} Details Page`,
-        description: 'Product Details Page',
-        keywords: 'Product Details, Product Details Page',
-    };
-
-}
-
-// with promise
-// export const generateMetadata =async ({ params } : props): Promise<Metadata> => {
-//
-//     const title = await new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve(`Product ${params.productId} Details Page`);
-//         }, 1000);
-//     });
-//
+// export const generateMetadata = ({ params } : props): Metadata => {
 //     return {
-//         title: `Product ${title} Details Page`,
+//         title: `Product ${params.productId} Details Page`,
 //         description: 'Product Details Page',
 //         keywords: 'Product Details, Product Details Page',
 //     };
 //
 // }
 
+// with promise
+export const generateMetadata =async ({ params } : props): Promise<Metadata> => {
 
-const page = ({ params } : props) => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Product ${params.productId} Details Page`);
+        }, 1000);
+    });
+
+    return {
+        title: `Product ${title} Details Page`,
+        description: 'Product Details Page',
+        keywords: 'Product Details, Product Details Page',
+    };
+
+}
+
+
+const Page = ({ params } : props) => {
     return (
-        <>Product Details ({params.productId})</>
+        <>Product Details ({params.productId})
+            <ul>
+                {Array.from({length: 5}).map((_, i) => (
+                    <li key={i}><Link href={`/products/${params.productId}/reviews/${i}`}> Review - {i} </Link></li>
+                )
+                )}
+            </ul>
+        </>
     );
 };
 
-export default page;
+export default Page;
